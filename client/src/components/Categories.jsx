fa8bd527bef29b1e5b90a463630b18fd101c5c23
M-Barath-Vikraman <barathvikraman008@gmail.com>
2025-01-20 18:01:29 +0530

    first commit

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,33 @@
+import { categories } from "../data";
+import "../styles/Categories.scss"
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <h1>Explore Top Categories</h1>
+      <p>
+      Discover an array of vacation homes designed for every traveler.
+      Dive into the heart of local culture, unwind in cozy comfort, 
+      and make lasting memories in your perfect getaway.
+      </p>
+
+      <div className="categories_list">
+        {categories?.slice(1,5).map((category, index) => (
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
+              <img src={category.img} alt={category.label} />
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,15 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>  
+      Step Inside, No Matter Where You Are. <br /> 
+      Live the adventure, treasure the now, 
+      and let your memories shine forever
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
